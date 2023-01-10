b2cc970ff20c320c00b231abe488c0610e260786
skango <65347282+skango@users.noreply.github.com>
2023-01-10 22:56:50 +0400

    Navigation

--- app/src/main/res/onCreate.kt
+++ app/src/main/res/onCreate.kt
@@ -1,16 +1,4 @@
-package com.example.rock_paper_scissors
-
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.navigation
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
-
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+ override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -30,4 +18,3 @@ class MainActivity : AppCompatActivity() {
 
 
     }
-}
